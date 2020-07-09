f98074cfe83379012216d5d3008a52667771fa2c
nothing628 <yogyphang@gmail.com>
2020-07-09 13:37:54 +0700

    introduce session

--- config.php
+++ config.php
@@ -5,3 +5,7 @@ $MYSQL_USER = "drivethru";
 $MYSQL_PASSWORD = "secret";
 $MYSQL_PORT = 3307;
 $MYSQL_HOST = "127.0.0.1";
+
+session_start();
+
+if (!isset($_SESSION['start_time'])) $_SESSION['start_time'] = new DateTime();
--- index.php
+++ index.php
@@ -6,5 +6,6 @@ ob_start();
 include('views/header.php');
 echo "Hi world";
 echo $MYSQL_USER;
+echo $_SESSION['start_time']->format('Y-m-d H:i:s');
 include('views/footer.php');
 ob_end_flush();
