827701574fd04a43b2fa83ac47dc0ea25e5fd00a
nothing628 <yogyphang@gmail.com>
2020-07-22 00:50:50 +0700

    add database dump

---
+++ dump.sql
@@ -0,0 +1,138 @@
+-- MariaDB dump 10.17  Distrib 10.5.4-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: drivethru
+-- ------------------------------------------------------
+-- Server version       10.5.4-MariaDB-1:10.5.4+maria~focal
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `karyawan`
+--
+
+DROP TABLE IF EXISTS `karyawan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `karyawan` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `jenis_karyawan` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+LOCK TABLES `karyawan` WRITE;
+/*!40000 ALTER TABLE `karyawan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `karyawan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produk`
+--
+
+DROP TABLE IF EXISTS `produk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produk` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nama_produk` varchar(100) NOT NULL,
+  `stok` int(10) unsigned NOT NULL DEFAULT 0,
+  `harga` decimal(25,0) NOT NULL,
+  `file_foto` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `produk_stok_IDX` (`stok`) USING BTREE,
+  KEY `produk_harga_IDX` (`harga`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produk`
+--
+
+LOCK TABLES `produk` WRITE;
+/*!40000 ALTER TABLE `produk` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produk` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaksi`
+--
+
+DROP TABLE IF EXISTS `transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaksi` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `waktu_transaksi` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` varchar(20) NOT NULL,
+  `total` decimal(25,0) NOT NULL DEFAULT 0,
+  `pajak` decimal(25,0) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+LOCK TABLES `transaksi` WRITE;
+/*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaksi_detail`
+--
+
+DROP TABLE IF EXISTS `transaksi_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaksi_detail` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_transaksi` bigint(20) unsigned NOT NULL,
+  `id_produk` bigint(20) unsigned NOT NULL,
+  `qty` int(10) unsigned NOT NULL DEFAULT 0,
+  `price` decimal(25,0) NOT NULL DEFAULT 0,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transaksi_detail_FK` (`id_transaksi`),
+  KEY `transaksi_detail_FK_1` (`id_produk`),
+  CONSTRAINT `transaksi_detail_FK` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id`),
+  CONSTRAINT `transaksi_detail_FK_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaksi_detail`
+--
+
+LOCK TABLES `transaksi_detail` WRITE;
+/*!40000 ALTER TABLE `transaksi_detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaksi_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-21 17:49:14
